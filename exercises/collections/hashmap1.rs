089e490db9ce0bf97cf9f6d9c8fe33d2eee2e7b4
Tatiana <tarzhakova@gmail.com>
2021-11-18 12:42:24 +0100

    solutions until generics3.rs

--- exercises/collections/hashmap1.rs
+++ exercises/collections/hashmap1.rs
@@ -11,19 +11,21 @@
 // Execute the command `rustlings hint hashmap1` if you need
 // hints.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new();// TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
 
-    basket
+    basket.insert(String::from("apple"), 5);
+    basket.insert(String::from("pear"), 5);
+    return basket;
 }
 
 #[cfg(test)]
--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -10,13 +10,19 @@
 // No boiler plate code this time,
 // you can do this!
 
-// I AM NOT DONE
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 //TODO: Add your code here
+impl AppendBar for Vec<String> {
+    //Add your code here
+    fn append_bar(mut self) -> Self {
+        self.push("Bar".to_string());
+        self
+    }
+}
 
 #[cfg(test)]
 mod tests {
